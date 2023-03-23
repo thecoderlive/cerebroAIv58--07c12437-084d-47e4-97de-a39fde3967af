b0a2bf687515d6cfe806daa389e52122522c6f23
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-23 13:57:06 +0530

    Initial commit for file: api.js

---
+++ api.js
@@ -0,0 +1,4 @@
+export const api = {
+  "home": "https://script.google.com/macros/s/AKfycbykuRcoaBFTiHjs3NYvWXI4xv8GV0olBr1Sp0O_vq4Y113HB59uAdUhbXID4qpaUJJF/exec?method=list&endpoint=home&screen_name=home&ss_url=https%3A//docs.google.com/spreadsheets/d/1rmjgDEVXqJzW3Ky8MTnHhw3nd18jnUxhbRuGoOLJwdM/edit&item_id=",
+  "chats": "https://script.google.com/macros/s/AKfycbykuRcoaBFTiHjs3NYvWXI4xv8GV0olBr1Sp0O_vq4Y113HB59uAdUhbXID4qpaUJJF/exec?method=list&endpoint=chats&screen_name=home&ss_url=https%3A//docs.google.com/spreadsheets/d/1rmjgDEVXqJzW3Ky8MTnHhw3nd18jnUxhbRuGoOLJwdM/edit&item_id="
+}
